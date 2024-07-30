4181a81547a7dfe141e5aa63e4813729a9c484db
falconmaverick <39731035+falconmaverick@users.noreply.github.com>
2024-07-30 12:24:03 +0300

    Create chessboard.js

---
+++ chessboard.js
@@ -0,0 +1,2 @@
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/chessboardjs/1.0.0/chessboard-1.0.0.min.css">
+<script src="https://cdnjs.cloudflare.com/ajax/libs/chessboardjs/1.0.0/chessboard-1.0.0.min.js"></script>
